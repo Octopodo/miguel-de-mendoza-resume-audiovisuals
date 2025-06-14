1c49cb40bc40ba8726a9c77eea9eb1e0a72dd8af
Octopodo <orson_wells40@hotmail.com>
2025-06-14 12:03:51 +0200

    First commit

---
+++ astro.config.mjs
@@ -0,0 +1,10 @@
+// @ts-check
+import { defineConfig } from 'astro/config';
+
+// https://astro.build/config
+export default defineConfig({
+    site: 'https://Octopodo.github.io',
+    base: '/cv-2025',
+    output: 'static',
+    outDir: 'docs'
+});
